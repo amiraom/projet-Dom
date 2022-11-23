a5dbe0ebef88e7deae2afbd3fdd5794e4452a7ed
amira <amira.omrani6@gmail.com>
2022-11-23 16:29:37 +0100

    first commit

--- index.js
+++ index.js
@@ -192,3 +192,13 @@ delete3.addEventListener("click",function del() {
 delete4.addEventListener("click",function del() {
     delete4.parentElement.parentElement.remove()
 })
+var phrase="Each character will be treated separately.";
+var  i,j ;
+var nbc=0;
+for(i=0;i<phrase.length;i++)
+{
+
+    nbc=nbc+i;
+
+}
+console.log(nbc);
---
+++ test.js
@@ -0,0 +1,10 @@
+var phrase="Each character will be treated separately.";
+var  i,j ;
+var nbc=0;
+for(i=0;i<phrase.length;i++)
+{
+
+    nbc=nbc+i;
+
+}
+console.log(nbc);
